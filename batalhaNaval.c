5d7a76db344cdf18292ea734658d7b661432f7b4
YuriPEPELAU <yurialvesherdy@gmail.com>
2025-03-26 18:44:48 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+int main(){
+    // Bloco de decalaração de matrizes e vetores.
+    char* coluna[10]={"   A ","B ","C ","D ","E ","F ","G ","H ","I ","J "};
+    int tabuleiro[10][10];
+    char* linha[10]={"1. " , "2. " , "3. " , "4. " , "5. " , "6. " , "7. " , "8. " , "9. " , "10."};
+    // Impressão do nome do jogo
+    printf("\nTABULEIRO DE BATALHA NAVAL \n");
+    // Estrutura de repetição 'for' para impressão das letras que representam as colunas.
+    for(int a=0;a<10;a++){
+        printf("%s",coluna[a]);
+    }
+    printf("\n");
+    // Estrutura de repetição 'for' para impressão dos números que representam as linhas.
+    for(int i=0;i<10;i++){
+        printf("%s",linha[i]);
+        // Estrutura de repetição 'for' para impressão da matriz com os zeros representando a água e os três representam os barcos.
+        for(int j=0;j<10;j++){
+            tabuleiro[i][j]='0';
+            tabuleiro[5][5]='3';
+            tabuleiro[5][6]='3';
+            tabuleiro[5][7]='3';
+            tabuleiro[1][1]='3';
+            tabuleiro[2][1]='3';
+            tabuleiro[3][1]='3';
+            printf("%c ",tabuleiro[i][j]);
+        }
+        
+        printf("\n");
+    }
+    return 0;    
 }
